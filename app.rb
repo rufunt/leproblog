d2b99f93fde5de67e3c0ad66d7773a39790eb9d3
rufunt <jazzoo@mail.ru>
2020-06-23 20:49:29 +0300

    Init database

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'lepra.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
 
 
 get '/' do
