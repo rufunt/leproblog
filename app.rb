f4a3234d5dbfad528619a2894165c3755edf64f0
rufunt <jazzoo@mail.ru>
2020-07-07 00:16:02 +0300

    Create table Comments

--- app.rb
+++ app.rb
@@ -19,7 +19,15 @@ configure do
     id integer primary key autoincrement, 
     created_date date, 
     content text
-  )'  
+  )' 
+  
+  @db.execute 'create table if not exists Comments
+  (
+    id integer primary key autoincrement, 
+    created_date date, 
+    content text,
+    post_id integer
+  )'
 end
 
 
